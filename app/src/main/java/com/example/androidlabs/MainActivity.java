7c0285c68c2da3c1618e9ad66a46d98996319172
DamYash <damansyash@gmail.com>
2021-10-04 18:03:08 -0400

    Finished Lab3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     EditText email;
     String uEmail;
+    Button button;
 
 
     @Override
@@ -21,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.login);
 
         email = findViewById(R.id.textbox1);
-        SharedPreferences sharedPreferences = getSharedPreferences("sharedPreferences", Context.MODE_PRIVATE);
+        button = findViewById(R.id.loginbutton);
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         uEmail = sharedPreferences.getString("email", "");
         email.setText(uEmail);
 
@@ -43,9 +46,12 @@ public class MainActivity extends AppCompatActivity {
         protected void onPause(){
             super.onPause();
             uEmail = email.getText().toString();
-            SharedPreferences sharedPreferences1 = getSharedPreferences("sharedPreferences1", Context.MODE_PRIVATE);
+            SharedPreferences sharedPreferences1 = getSharedPreferences("MyPreferences1", Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPreferences1.edit();
+            editor.putString("email", uEmail);
             editor.apply();
+
+            Toast.makeText(MainActivity.this, "Email address saved", Toast.LENGTH_LONG).show();
         }
 
 

c3962512a111dcec54224972ce4c1003cb1d2623
DamYash <damansyash@gmail.com>
2021-10-10 11:50:56 -0400

    Lab 3 updated acc to the solutions

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,61 +1,118 @@
 package com.example.androidlabs;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    EditText email;
-    String uEmail;
-    Button button;
+    private static final String SHARED_PREFS = "lab3";
+    private static final String EMAIL_PREF_KEY = "email";
 
+    private EditText emailText;
+    private Button login;
+    private SharedPreferences prefs;
+    private String savedEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_main);
 
-        email = findViewById(R.id.textbox1);
-        button = findViewById(R.id.loginbutton);
-        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        uEmail = sharedPreferences.getString("email", "");
-        email.setText(uEmail);
-
-
-        Button button = (Button) findViewById(R.id.loginbutton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ProfileActivity();
-            }
+        // Check the shared preferences for a saved email and display it in the email box, otherwise display an empty string.
+        SharedPreferences pref = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String savedEmail = pref.getString(EMAIL_PREF_KEY, "");
+        ((EditText) findViewById(R.id.email)).setText(savedEmail);
+        // When the login button is clicked, transition to the Profile activity.
+        ((Button) findViewById(R.id.login)).setOnClickListener(clk -> {
+            // Saving the email to the shared preferences while clicking the button wasn't required, but it's helpful.
+            saveEmailToPreferences();
+            startActivity(new Intent(this, ProfileActivity.class));
         });
     }
-
-
-    public void ProfileActivity(){
-        Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
+    private void saveEmailToPreferences() {
+        SharedPreferences pref = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String enteredEmail = ((EditText) findViewById(R.id.email)).getText().toString();
+        pref.edit().putString(EMAIL_PREF_KEY, enteredEmail).commit();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
-        protected void onPause(){
-            super.onPause();
-            uEmail = email.getText().toString();
-            SharedPreferences sharedPreferences1 = getSharedPreferences("MyPreferences1", Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences1.edit();
-            editor.putString("email", uEmail);
-            editor.apply();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Ensure the contents of the email box are saved in the shared preferences
+        saveEmailToPreferences();
+    }
 
-            Toast.makeText(MainActivity.this, "Email address saved", Toast.LENGTH_LONG).show();
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
-
-
-
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
+
+
+
+
+
+
+
+/**EditText email;
+String uEmail;
+Button button;
+
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.login);
+
+    email = findViewById(R.id.textbox1);
+    button = findViewById(R.id.loginbutton);
+    SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+    uEmail = sharedPreferences.getString("email", "");
+    email.setText(uEmail);
+
+
+    Button button = (Button) findViewById(R.id.loginbutton);
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            ProfileActivity();
+        }
+    });
+}
+
+
+public void ProfileActivity(){
+    Intent intent = new Intent(this, ProfileActivity.class);
+    startActivity(intent);
+}
+
+    protected void onPause(){
+        super.onPause();
+        uEmail = email.getText().toString();
+        SharedPreferences sharedPreferences1 = getSharedPreferences("MyPreferences1", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences1.edit();
+        editor.putString("email", uEmail);
+        editor.apply();
+
+        Toast.makeText(MainActivity.this, "Email address saved", Toast.LENGTH_LONG).show();
+    }**/
+
+
+
+
--- app/src/main/java/com/example/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -1,19 +1,83 @@
 package com.example.androidlabs;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String ACTIVITY_NAME = "ProfileActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        // When the picture button is clicked, start the camera intent
+        ((ImageButton) findViewById(R.id.profile_picture)).setOnClickListener(clk -> {
+            dispatchTakePictureIntent();
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ((ImageButton) findViewById(R.id.profile_picture)).setImageBitmap(imageBitmap);
+        }
+        Log.e(ACTIVITY_NAME, "in function" + "onActivityResult();");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "in function" + "onStart();");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "in function" + "onPause();");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "in function" + "onResume();");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "in function" + "onStop;");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "in function" + "onDestroy();");
+    }
+}
+
+
+
+
+
+/**public class ProfileActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     public static final String ACTIVITY_NAME = "ProfileActivity";
@@ -22,6 +86,7 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
         int e = Log.e(ACTIVITY_NAME, "In OnCreate");
         TextView t1;
 
@@ -40,6 +105,19 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
             }
         });
+
+        Button button1 = (Button) findViewById(R.id.chatbutton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ChatRoomActivity();
+            }
+        });
+    }
+    
+    public void ChatRoomActivity(){
+        Intent intent = new Intent(this, ChatRoomActivity.class);
+        startActivity(intent);
     }
 
     protected void onStart(){
@@ -82,4 +160,4 @@ public class ProfileActivity extends AppCompatActivity {
 
 
 
-}
+}**/
